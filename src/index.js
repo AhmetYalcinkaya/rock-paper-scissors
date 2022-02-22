3be040d34ccdc6a3218fb9364d567468cce17d44
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-02-22 13:33:09 +0300

    y

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.scss";
-
+import { BrowserRouter } from "react-router-dom";
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  mountNode
+);
